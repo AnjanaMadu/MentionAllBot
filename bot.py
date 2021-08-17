ce6fea3a845f2ae97c4f598e8f50d044dc8fc452
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-08-17 18:54:58 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,79 @@
+import os, logging, asyncio
+from telethon import Button
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from telethon.tl.types import ChannelParticipantsAdmins
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(name)s - [%(levelname)s] - %(message)s'
+)
+LOGGER = logging.getLogger(__name__)
+
+api_id = int(os.environ.get("APP_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("TOKEN")
+client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
+
+@client.on(events.NewMessage(pattern="^/start$"))
+async def start(event):
+  await event.reply("__**Im MentionAll Bot**, I can mention almost all members in group or channel 👻\nClick **/help** for more infomation__\n\n Follow [@AnjanaMadu](https://github.com/AnjanaMadu) on Github",
+                    buttons=(
+                      [Button.url('📣 Channel', 'https://t.me/harp_tech')],
+                      [Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')]
+                    )
+                   )
+@client.on(events.NewMessage(pattern="^/help$"))
+async def help(event):
+  helptext = "**Help Menu of MentionAllBot**\n\nCommand: /mentioall\n__You can use this command with text what you want to mention others.__\n`Example: /mentionall Good Morning!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__."
+  await event.reply(helptext,
+                    buttons=(
+                      [Button.url('📣 Channel', 'https://t.me/harp_tech')],
+                      [Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')]
+                    )
+                   )
+  
+@client.on(events.NewMessage(pattern="^/mentionall ?(.*)"))
+async def mentionall(event):
+  if event.is_private:
+    return await event.respond("__This command can be use in groups and channels!__")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond("__Only admins can mention all!__")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+  else:
+    return await event.respond("__Reply to a message or give me some text to mention others!__")
+  
+  if mode == "text_on_cmd":
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+      if usrnum == 5:
+        await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
+        usrnum = 0
+        usrtxt = ""
+        
+  if mode == "text_on_reply":
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+      if usrnum == 5:
+        await client.send_message(event.chat_id, usrtxt, reply_to=msg)
+        usrnum = 0
+        usrtxt = ""
+        
+  print(">> BOT STARTED <<")
+  client.run_until_disconnected()
